25788d98b43919c3d9277e9af566f6e6342f072a
Yang Wang <gnavvy@gmail.com>
2013-06-27 15:16:01 -0700

    footer refined

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 ﻿<footer id="footer-bottom">
 	<div class="container">
-		<div class="five columns offset-by-twelve">
-			<div class="copyright">© 2013 <a href="#">UCDavis CSSA</a>. All Rights Reserved.</div>
-		</div>
+        <div class="copyright">© 2013 <a href="#">UCDavis CSSA</a>. All Rights Reserved.</div>
 	</div>
 </footer>
 
